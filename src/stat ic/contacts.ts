1490383a23e661797a1af311318191c4d7fc5963
nheingit <nheindev@gmail.com>
2021-05-07 21:38:29 -0500

    first commit

---
+++ src/stat ic/contacts.ts
@@ -0,0 +1,27 @@
+interface Contact {
+    firstName: string;
+    lastName: string;
+    phoneNumber: number;
+    email: string;
+}
+
+const contacts:Array<Contact> = [
+    {
+        "firstName": "Lamar",
+        "lastName": "Brown",
+        "phoneNumber": 5124009351,
+        "email": "LBrown@gmail.com",
+    },
+    {
+        "firstName": "Lamar",
+        "lastName": "Brown",
+        "phoneNumber": 5124009351,
+        "email": "LBrown@gmail.com",
+    },
+    {
+        "firstName": "John",
+        "lastName": "Smith",
+        "phoneNumber": 3132590583,
+        "email": "JSmith@yahoo.com",
+    },
+]
